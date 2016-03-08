cd253fd18197869a54f470f024ed9c8829f1271c
Gordon Lesti <info@gordonlesti.com>
2016-03-08 12:13:17 +0100

    add bootstrap.php

---
+++ Test/Unit/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © PART <info@part-online.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
+
+ function __()
+ {
+     return $argc = func_get_args();
+ }
